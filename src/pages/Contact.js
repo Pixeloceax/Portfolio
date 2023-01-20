f6f738f131fc58907b512d7b30eefbb5c5daac85
Axel <axel.valentinbvs@yahoo.com>
2023-01-20 21:47:10 +0000

    upadte files

--- src/pages/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const contact = () => {
-  return <></>;
-};
-
-export default contact;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+// import css
+import "../styles/pages/Contact.css";
+
+const contact = () => {
+  return (
+    <>
+      <main className="primary-background-color w-100 h-100 d-flex flex-column align-items-center">
+        <section id="contact">
+          <div className="d-flex flex-column align-items-center m-0 mt-5 pt-5">
+            <h1 className="text-color text-center mb-3 fw-bold">
+              Let's have a talk
+            </h1>
+          </div>
+
+          <div className="d-flex flex-column align-items-center m-0 mt-5 pt-5"></div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default contact;
