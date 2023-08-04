c35abe315a21bec2415af9bbebe572d7657d5500
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-08-04 14:06:52 +0200

    fix about me section

--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -9,67 +9,67 @@ import github from "../../assets/icons/tools/github-icon.svg";
 const About = () => {
   return (
     <>
-      <div className="about__bg-container">
-        <img src={axelValentin} alt="template" />
-      </div>
-
-      <CircleTextAnimation />
-
-      <div className="about__inner">
-        <div className="about__content">
-          <div className="about__badge">
-            <div className="about__badge-name">About me</div>
-            <div className="about__badge-bottom">
-              <span>I'm a Full Stack engineer</span>
+      <div className="about__container">
+        <div className="about__bg__container">
+          <img src={axelValentin} alt="axelvalentin" />
+        </div>
+        <CircleTextAnimation />
+        <div className="about__inner">
+          <div className="about__content">
+            <div className="about__badge">
+              <div className="about__badge-name">About me</div>
+              <div className="about__badge-bottom">
+                <span>I'm a Full Stack engineer</span>
+              </div>
             </div>
-          </div>
-          <div className="about__text">
-            <p className="blurb">
-              I'm a 20-year-old Full Stack engineer based in Lille, France,
-              specializing in Back End development.
-            </p>
-          </div>
-
-          <div className="about__text">
-            <p className="blurb">
-              I have a passion for learning new JavaScript technologies.
-            </p>
-          </div>
-          <div className="about__text">
-            <p>
-              <span className="blurb">
-                Hobbies : Cat, Manga, Video game, Domotics.
-              </span>
-            </p>
-          </div>
-
-          <div className="about__spotify">
-            <div>
-              <p className="blurb">my favorite group :</p>
+            <div className="about__text">
+              <p className="blurb">
+                I'm a 20-year-old Full Stack engineer based in Lille, France,
+                specializing in Back End development.
+              </p>
             </div>
-            <iframe
-              title="spotify"
-              src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO3RbzfW?utm_source=generator&theme=0"
-              width="70%"
-              height="152"
-            />
-          </div>
 
-          <div className="about__social">
-            <a
-              href="https://www.linkedin.com/in/axelvalentin/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedin} alt="linkedin" width={30} height={30} />
-            </a>
-            <a
-              href="https://github.com/Pixeloceax"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={github} alt="github" width={30} height={30} />
-            </a>
+            <div className="about__text">
+              <p className="blurb">
+                I have a passion for learning new JavaScript technologies.
+              </p>
+            </div>
+            <div className="about__text">
+              <p>
+                <span className="blurb">
+                  Hobbies : Cat, Manga, Video game, Domotics.
+                </span>
+              </p>
+            </div>
+
+            <div className="about__spotify">
+              <div>
+                <p className="blurb">my favorite group :</p>
+              </div>
+              <iframe
+                title="spotify"
+                src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO3RbzfW?utm_source=generator&theme=0"
+                width="70%"
+                height="152"
+              />
+            </div>
+
+            <div className="about__social">
+              <a
+                href="https://www.linkedin.com/in/axelvalentin/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedin} alt="linkedin" width={30} height={30} />
+              </a>
+              <a
+                href="https://github.com/Pixeloceax"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={github} alt="github" width={30} height={30} />
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.min.css";
-import "./styles/section/main.min.css";
-import "./styles/components/ticker.min.css";
-import "./styles/components/circletext.min.css";
-import "./styles/components/card.min.css";
-import "./styles/section/about.min.css";
-import "./styles/section/project.min.css";
-import "./styles/section/contact.min.css";
-import "./styles/components/navbar.min.css";
-import "./styles/section/footer.min.css";
+import "./styles/index.css";
+import "./styles/section/main.css";
+import "./styles/components/ticker.css";
+import "./styles/components/circletext.css";
+import "./styles/components/card.css";
+import "./styles/section/about.css";
+import "./styles/section/project.css";
+import "./styles/section/contact.css";
+import "./styles/components/navbar.css";
+import "./styles/section/footer.css";
 
 import App from "./App";
 
