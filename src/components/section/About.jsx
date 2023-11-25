3b9ef1f9910f0fe2b254e12257f4d43ff7bc73b0
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-25 16:21:55 +0100

    Complete overhaul of the page to enhance responsiveness and update information.

--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -9,67 +9,71 @@ import github from "../../assets/icons/tools/github-icon.svg";
 const About = () => {
   return (
     <>
-      <div className="about__container">
-        <div className="about__bg__container">
-          <img src={axelValentin} alt="axelvalentin" />
+      <div className="about__me__container">
+        <div className="about__me__image__container">
+          <img
+            src={axelValentin}
+            alt="Description de l'image"
+            className="profile-image"
+          />
         </div>
-        <CircleTextAnimation />
-        <div className="about__inner">
-          <div className="about__content">
-            <div className="about__badge">
-              <div className="about__badge-name">About me</div>
-              <div className="about__badge-bottom">
-                <span>I'm a Full Stack engineer</span>
-              </div>
-            </div>
-            <div className="about__text">
-              <p className="blurb">
-                I'm a 20-year-old Full Stack engineer based in Lille, France,
-                specializing in Back End development.
-              </p>
+
+        <div className="circle__text__animation">
+          <CircleTextAnimation />
+        </div>
+
+        <div className="about__me__text__container">
+          <h2>About me</h2>
+
+          <div className="about__text">
+            <p className="blurb">
+              I'm a 20-year-old <b> Full Stack engineer </b> based in{" "}
+              <b> Lille</b>, France, with a strong focus on Back End
+              development.
+            </p>
+          </div>
+
+          <div className="about__text">
+            <p className="blurb">
+              I'm currently looking for an <b> internship</b> in France.
+            </p>
+          </div>
+
+          <div className="about__text">
+            <p className="blurb">
+              My hobbies are video Cats, read CyberPunk books and play FPS video
+              games.
+            </p>
+          </div>
+
+          <div className="about-spotify">
+            <div>
+              <p className="blurb">My favorite group:</p>
             </div>
 
-            <div className="about__text">
-              <p className="blurb">
-                I have a passion for learning new JavaScript technologies.
-              </p>
-            </div>
-            <div className="about__text">
-              <p>
-                <span className="blurb">
-                  Hobbies : Cat, Manga, Video game, Domotics.
-                </span>
-              </p>
-            </div>
+            <iframe
+              title="spotify"
+              src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO3RbzfW?utm_source=generator&theme=0"
+              width="70%"
+              height="152"
+            />
+          </div>
 
-            <div className="about__spotify">
-              <div>
-                <p className="blurb">my favorite group :</p>
-              </div>
-              <iframe
-                title="spotify"
-                src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO3RbzfW?utm_source=generator&theme=0"
-                width="70%"
-                height="152"
-              />
-            </div>
-
-            <div className="about__social">
-              <a
-                href="https://www.linkedin.com/in/axelvalentin/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={linkedin} alt="linkedin" width={30} height={30} />
-              </a>
-              <a
-                href="https://github.com/Pixeloceax"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={github} alt="github" width={30} height={30} />
-              </a>
-            </div>
+          <div className="about__social">
+            <a
+              href="https://www.linkedin.com/in/axelvalentin/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedin} alt="linkedin" width={30} height={30} />
+            </a>
+            <a
+              href="https://github.com/Pixeloceax"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="github" width={30} height={30} />
+            </a>
           </div>
         </div>
       </div>
